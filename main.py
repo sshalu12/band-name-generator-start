990b3ca9cb3cf6421013a2d9723971180ce112c3
sshalu12 <57828137+sshalu12@users.noreply.github.com>
2021-05-22 12:00:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#1. Create a greeting for your program.
+
 print("Welcome to Band Name Geneator. ")
 
-#2. Ask the user for the city that they grew up in.
+
 a=input("What's the name of city you grew up in? \n")
-#3. Ask the user for the name of a pet.
+
 b=input("What's your pet name? \n")
-#4. Combine the name of their city and pet and show them their band name.
+
 print("Your bans name could be "+a+" "+b)
