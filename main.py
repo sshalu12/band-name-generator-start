9188e1f6d759dcdb87c1f5c1937fb02b3e627c9a
Shalini <57828137+sshalu12@users.noreply.github.com>
2021-06-02 00:28:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+#Band Name Generator
+#In this program user has to input the city and the pet name to generate band name 
 
 print("Welcome to Band Name Geneator. ")
-
-
 a=input("What's the name of city you grew up in? \n")
-
 b=input("What's your pet name? \n")
-
-print("Your bans name could be "+a+" "+b)
+print("Your band name could be "+a+" "+b)
